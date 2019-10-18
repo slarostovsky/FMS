849e8eef88a74c6fe52ec63b1a7c4056dd047d99
Slava <slarostovsky@gmail.com>
2019-10-18 19:58:22 +0300

    Ver2.0 Desc compl

--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 2000,
         sliderToShow:1,
-        arrows: false
-        /* prevArrow: '<button type="button" class="prev"><img src="images/img/left_arrow.png"></button>',
-        nextArrow: '<button type="button" class="next"><img src="images/img/right_arrow.png"></button>', */
+        arrows: true,
+        prevArrow: '<button type="button" class="prev"><img src="icons/arrows/arrow_left_icon.png"></button>',
+        nextArrow: '<button type="button" class="next"><img src="icons/arrows/arrow_right_icon.png"></button>',
         /* responsive: [{
             breakpoint: 992,
             settings: {
@@ -14,4 +14,21 @@ $(document).ready(function () {
             }
         }] */
     });
+
+    $('.solutions__carousel').slick({
+        speed: 1200,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        sliderToShow:1,
+        arrows: true,
+        prevArrow: '<button type="button" class="prev_slt"><img src="icons/arrows/arrow_slt_left.png"></button>',
+        nextArrow: '<button type="button" class="next_slt"><img src="icons/arrows/arrow_slt_right.png"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+            }
+        }]
+    });
+    
 });
